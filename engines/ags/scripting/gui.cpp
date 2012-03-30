09425ae93b50da20693cd79ca1506a98b179f921
Alyssa Milburn <fuzzie@fuzzie.org>
2012-03-30 20:57:04 +0200

    AGS: Call resized() from SetInvDimensions.

--- engines/ags/scripting/gui.cpp
+++ engines/ags/scripting/gui.cpp
@@ -126,7 +126,7 @@ RuntimeValue Script_SetInvDimensions(AGSEngine *vm, ScriptObject *, const Common
 
 	// backwards compatibility
 	for (uint i = 0; i < vm->_gameFile->_guiInvControls.size(); ++i) {
-		vm->_gameFile->_guiInvControls[i]->resize(width, height);
+		vm->_gameFile->_guiInvControls[i]->resized();
 	}
 
 	vm->invalidateGUI();
