ae8321b54a8c09fb2df101e1c5dda6a5bc2ff3bc
rofl0r <retnyg@gmx.net>
2012-11-18 22:02:29 +0100

    AGS: implement SetCharacterProperty

--- engines/ags/scripting/character.cpp
+++ engines/ags/scripting/character.cpp
@@ -126,7 +126,7 @@ RuntimeValue Script_GetCharacterProperty(AGSEngine *vm, ScriptObject *, const Co
 	ScriptString *property = (ScriptString *)params[1]._object;
 
 	if (charid >= vm->_characters.size())
-		error("GetCharacterPropertyText: character %d is too high (only have %d)", charid, vm->_characters.size());
+		error("GetCharacterProperty: character %d is too high (only have %d)", charid, vm->_characters.size());
 
 	return vm->getIntProperty(property->getString(), vm->_characters[charid]->_properties);
 }
@@ -860,15 +860,14 @@ RuntimeValue Script_SetCharacterIgnoreWalkbehinds(AGSEngine *vm, ScriptObject *,
 // Obsolete character function.
 RuntimeValue Script_SetCharacterProperty(AGSEngine *vm, ScriptObject *, const Common::Array<RuntimeValue> &params) {
 	uint32 charid = params[0]._value;
-	UNUSED(charid);
-	int property = params[1]._signedValue;
-	UNUSED(property);
+	int flag = params[1]._signedValue;
 	int newValue = params[2]._signedValue;
-	UNUSED(newValue);
-
-	// FIXME
-	error("SetCharacterProperty unimplemented");
-
+	
+	if (charid >= vm->_characters.size())
+		error("GetCharacterProperty: character %d is too high (only have %d)", charid, vm->_characters.size());
+	
+	Character *ch = vm->_characters[charid];
+	ch->SetOption(flag, newValue);
 	return RuntimeValue();
 }
 
